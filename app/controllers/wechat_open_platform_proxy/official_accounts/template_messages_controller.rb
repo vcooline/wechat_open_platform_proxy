9f0445cabb513354754467f3b45e89579b933e07
Andersen Fan <as181920@gmail.com>
2018-11-03 12:31:36 +0800

    Add ip whitelist check for template message

--- app/controllers/wechat_open_platform_proxy/official_accounts/template_messages_controller.rb
+++ app/controllers/wechat_open_platform_proxy/official_accounts/template_messages_controller.rb
@@ -2,6 +2,8 @@ require_dependency "wechat_open_platform_proxy/application_controller"
 
 module WechatOpenPlatformProxy
   class OfficialAccounts::TemplateMessagesController < OfficialAccounts::BaseController
+    before_action :check_remote_ip_whitelisted, only: [:create]
+
     def create
       resp = OfficialAccountTemplateMessageService.new(@official_account).send_message(template_message_params)
       render json: JSON.load(resp.body), status: resp.status
