a275f0618d76ea4b98f7cceac55a598ebb6c3bcd
Andersen Fan <as181920@gmail.com>
2018-11-03 08:42:42 +0800

    Enable refresh_account_info for official account

--- app/services/wechat_open_platform_proxy/official_account_authorize_service.rb
+++ app/services/wechat_open_platform_proxy/official_account_authorize_service.rb
@@ -31,6 +31,25 @@ module WechatOpenPlatformProxy
       end
     end
 
+    def refresh_account_info(app_id)
+      authorizer_info = get_authorizer_info(app_id)
+      third_party_platform.official_accounts.find_or_initialize_by(app_id: app_id).tap do |official_account|
+        official_account.update(
+          original_id: authorizer_info["user_name"],
+          nick_name: authorizer_info["nick_name"],
+          head_img: authorizer_info["head_img"],
+          qrcode_url: authorizer_info["qrcode_url"],
+          principal_name: authorizer_info["principal_name"],
+          service_type_info: authorizer_info["service_type_info"],
+          verify_type_info: authorizer_info["verify_type_info"],
+          business_info: authorizer_info["business_info"],
+          alias: authorizer_info["alias"],
+          signature: authorizer_info["signature"],
+          mini_program_info: authorizer_info["MiniProgramInfo"],
+        )
+      end
+    end
+
     private
       def get_authorization_info(auth_code)
         request_params = { component_appid: third_party_platform.app_id, authorization_code: auth_code }
