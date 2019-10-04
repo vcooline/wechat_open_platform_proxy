0b3518ef5117b363ca017261cad6082e01a8b713
Andersen Fan <as181920@gmail.com>
2019-10-04 19:11:41 +0800

    Add jssdk config api

--- app/controllers/wechat_open_platform_proxy/official_accounts/jssdk_controller.rb
+++ app/controllers/wechat_open_platform_proxy/official_accounts/jssdk_controller.rb
@@ -2,5 +2,23 @@ require_dependency "wechat_open_platform_proxy/application_controller"
 
 module WechatOpenPlatformProxy
   class OfficialAccounts::JssdkController < OfficialAccounts::BaseController
+    before_action :check_remote_ip_whitelisted, only: [:create]
+
+    def wx_config
+      render json: OfficialAccountJssdkService.new(@official_account).config(config_params[:url]), status: :ok
+    end
+
+    def card_wx_config
+      render json: OfficialAccountJssdkService.new(@official_account).card_ext_config(*card_config_params.values_at(:card_id, :code, :openid)), status: :ok
+    end
+
+    private
+      def config_params
+        params.fetch(:config_params, {}).permit(:url)
+      end
+
+      def card_config_params
+        params.fetch(:card_config_params, {}).permit(:card_id, :code, :openid)
+      end
   end
 end
