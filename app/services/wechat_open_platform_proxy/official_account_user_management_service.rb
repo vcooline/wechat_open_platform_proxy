bd20b18db2b9c3176e85e54e48f54832c211fbba
Andersen Fan <as181920@gmail.com>
2019-11-26 11:38:45 +0800

    Fix data lose of safe_response_body

--- app/services/wechat_open_platform_proxy/official_account_user_management_service.rb
+++ app/services/wechat_open_platform_proxy/official_account_user_management_service.rb
@@ -29,7 +29,7 @@ module WechatOpenPlatformProxy
     private
       def safe_response_body(resp)
         return "{}" if resp.body.blank?
-        resp.body.encode('UTF-8', invalid: :replace, undef: :replace, replace: '')
+        resp.body.force_encoding("UTF-8").encode('UTF-8', invalid: :replace, undef: :replace, replace: '')
       end
   end
 end
