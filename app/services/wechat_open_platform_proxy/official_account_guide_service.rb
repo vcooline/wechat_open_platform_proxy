4db2444f14179b9a02629bdf6ec08586c17179a2
Andersen Fan <as181920@gmail.com>
2020-06-09 11:13:36 +0800

    Add OfficialAccountGuideService with guide_list

---
+++ app/services/wechat_open_platform_proxy/official_account_guide_service.rb
@@ -0,0 +1,22 @@
+module WechatOpenPlatformProxy
+  class OfficialAccountGuideService
+    attr_reader :official_account
+
+    def initialize(official_account)
+      @official_account = official_account
+    end
+
+    def guide_list(params={})
+      Rails.logger.info "OfficialAccountGuideService guide_list reqt"
+      resp = Faraday.post "https://api.weixin.qq.com/cgi-bin/guide/getguideacctlist?access_token=#{access_token}", params.to_json
+      Rails.logger.info "OfficialAccountGuideService guide_list resp: #{resp.body.squish}"
+
+      JSON.load(resp.body)
+    end
+
+    private
+      def access_token
+        OfficialAccountCacheStore.new(official_account).fetch_access_token
+      end
+  end
+end
