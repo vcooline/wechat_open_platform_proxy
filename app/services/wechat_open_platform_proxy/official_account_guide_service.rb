45abcde35c8a4e64a823f460d78303372b80334b
Andersen Fan <as181920@gmail.com>
2020-06-09 18:19:25 +0800

    Add bind_guide_customer

--- app/services/wechat_open_platform_proxy/official_account_guide_service.rb
+++ app/services/wechat_open_platform_proxy/official_account_guide_service.rb
@@ -22,6 +22,14 @@ module WechatOpenPlatformProxy
       JSON.load(resp.body)
     end
 
+    def bind_guide_customer(params)
+      Rails.logger.info "OfficialAccountGuideService bind_guide_customer reqt: #{params.to_json}"
+      resp = Faraday.post "https://api.weixin.qq.com/cgi-bin/guide/updateguidebuyerrelation?access_token=#{access_token}", params.to_json
+      Rails.logger.info "OfficialAccountGuideService bind_guide_customer resp: #{resp.body.squish}"
+
+      resp
+    end
+
     private
       def access_token
         OfficialAccountCacheStore.new(official_account).fetch_access_token
