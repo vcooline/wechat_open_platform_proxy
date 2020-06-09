b16d48117967496512776a8a9d46bd0d17b7efe0
Andersen Fan <as181920@gmail.com>
2020-06-09 12:41:17 +0800

    Add guide_qrcode method

--- app/services/wechat_open_platform_proxy/official_account_guide_service.rb
+++ app/services/wechat_open_platform_proxy/official_account_guide_service.rb
@@ -14,6 +14,14 @@ module WechatOpenPlatformProxy
       JSON.load(resp.body)
     end
 
+    def guide_qrcode(params={})
+      Rails.logger.info "OfficialAccountGuideService guide_qrcode reqt"
+      resp = Faraday.post "https://api.weixin.qq.com/cgi-bin/guide/guidecreateqrcode?access_token=#{access_token}", params.to_json
+      Rails.logger.info "OfficialAccountGuideService guide_qrcode resp: #{resp.body.squish}"
+
+      JSON.load(resp.body)
+    end
+
     private
       def access_token
         OfficialAccountCacheStore.new(official_account).fetch_access_token
