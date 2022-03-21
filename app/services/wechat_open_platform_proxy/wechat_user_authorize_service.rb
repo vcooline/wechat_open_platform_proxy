20c53e00ff4fd489045082251cb8585bdba8a36f
Andersen Fan <as181920@gmail.com>
2022-03-21 15:40:58 +0800

    Update refresh_auth_info with user_info fetch

--- app/services/wechat_open_platform_proxy/wechat_user_authorize_service.rb
+++ app/services/wechat_open_platform_proxy/wechat_user_authorize_service.rb
@@ -21,28 +21,20 @@ module WechatOpenPlatformProxy
 
     def get_user_info(code)
       base_info = get_base_info(code)
-      query_params = {
-        openid: base_info["openid"],
-        lang: "zh_CN",
-        access_token: base_info["access_token"]
-      }
-      resp = Faraday.get "https://api.weixin.qq.com/sns/userinfo?#{query_params.to_query}"
-      Rails.logger.info "WechatUserAuthorizeService get_user_info resp: #{resp.body.squish}"
-      JSON.parse(resp.body).reverse_merge(base_info)
+      user_info = base_info_to_user_info(base_info)
+      base_info.merge(user_info)
     end
 
     def refresh_auth_info(refresh_token = nil)
       return unless refresh_token.present?
 
-      query_params = {
-        appid: official_account.app_id,
-        grant_type: "refresh_token",
-        refresh_token: refresh_token
-      }
+      query_params = { appid: official_account.app_id, grant_type: "refresh_token", refresh_token: refresh_token }
       resp = Faraday.get "https://api.weixin.qq.com/sns/oauth2/refresh_token?#{query_params.to_query}"
       Rails.logger.info "WechatUserAuthorizeService refresh_auth_info resp: #{resp.body.squish}"
 
-      JSON.parse(resp.body) # TODO get user info if scope is snsapi_userinfo
+      base_info = JSON.parse(resp.body)
+      user_info = base_info["scope"].eql?("snsapi_userinfo") ? base_info_to_user_info(base_info) : {}
+      base_info.merge(user_info)
     end
 
     def refresh_user_info(open_id)
@@ -61,5 +53,19 @@ module WechatOpenPlatformProxy
 
       JSON.parse(resp.body)#.dig("unionid")
     end
+
+    private
+
+      def base_info_to_user_info(base_info)
+        query_params = {
+          openid: base_info["openid"],
+          lang: "zh_CN",
+          access_token: base_info["access_token"]
+        }
+        resp = Faraday.get "https://api.weixin.qq.com/sns/userinfo?#{query_params.to_query}"
+        Rails.logger.info "WechatUserAuthorizeService base_info_to_user_info resp: #{resp.body.squish}"
+
+        JSON.parse(resp.body)
+      end
   end
 end
