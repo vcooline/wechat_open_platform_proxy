ce7b864ab20ead815d0a58069c2de39a8957ceff
Andersen Fan <as181920@gmail.com>
2022-03-30 21:51:41 +0800

    Refactor url shorten helper

--- app/controllers/concerns/wechat_open_platform_proxy/url_utility.rb
+++ app/controllers/concerns/wechat_open_platform_proxy/url_utility.rb
@@ -7,8 +7,10 @@ module WechatOpenPlatformProxy
     end
 
     private
-      def url_with_additional_params(url, params={})
-        Addressable::URI.parse(url).tap{ |uri| uri.query_values = params.with_indifferent_access.reverse_merge(uri.query_values) }.normalize.to_s
+
+      def url_with_additional_params(url, params = {})
+        # Addressable::URI.parse(url).tap{ |uri| uri.query_values = params.with_indifferent_access.reverse_merge(Hash(uri.query_values)) }.normalize.to_s
+        URI(url).tap { |uri| uri.query = Rack::Utils.parse_nested_query(uri.query).deep_merge(params).to_query.presence }.to_s
       end
   end
 end
