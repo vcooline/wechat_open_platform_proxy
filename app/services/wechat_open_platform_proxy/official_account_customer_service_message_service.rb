7e88348d04097729b793b374980ed15d039b8d53
Andersen Fan <as181920@gmail.com>
2023-04-25 20:56:49 +0800

    Implement set typing on wechat customer service message

--- app/services/wechat_open_platform_proxy/official_account_customer_service_message_service.rb
+++ app/services/wechat_open_platform_proxy/official_account_customer_service_message_service.rb
@@ -13,5 +13,23 @@ module WechatOpenPlatformProxy
 
       resp
     end
+
+    def set_typing(open_id:)
+      message_params = { touser: open_id, command: "Typing" }
+      Rails.logger.info "#{self.class.name} set typing reqt: #{JSON.dump(message_params)}"
+      resp = Faraday.post "https://api.weixin.qq.com/cgi-bin/message/custom/typing?access_token=#{OfficialAccountCacheStore.new(official_account).fetch_access_token}", JSON.dump(message_params)
+      Rails.logger.info "#{self.class.name} set typing resp: #{resp.body.squish}"
+
+      JSON.parse(resp.body)["errcode"]&.zero?
+    end
+
+    def cancel_typing(open_id:)
+      message_params = { touser: open_id, command: "CancelTyping" }
+      Rails.logger.info "#{self.class.name} cancel typing reqt: #{JSON.dump(message_params)}"
+      resp = Faraday.post "https://api.weixin.qq.com/cgi-bin/message/custom/typing?access_token=#{OfficialAccountCacheStore.new(official_account).fetch_access_token}", JSON.dump(message_params)
+      Rails.logger.info "#{self.class.name} cancel typing resp: #{resp.body.squish}"
+
+      JSON.parse(resp.body)["errcode"]&.zero?
+    end
   end
 end
